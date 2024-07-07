ba6cc6719b09ecc5d4c534eb0b81661837a29d64
Alain Jean <ajean@sugit.com.ar>
2024-07-07 10:23:46 -0400

    fix clear filter link in dashboard

--- resources/js/Components/SearchForm.jsx
+++ resources/js/Components/SearchForm.jsx
@@ -18,7 +18,9 @@ export default function SearchForm({admin}) {
     return (
         <div className="gap-1 w-full justify-between flex">
             <input onChange={(e) => setData('search', e.target.value)} value={data.search} id='search' type="text" placeholder="Search cars..." className="border rounded-lg p-2 w-3/4" />
-            <button onClick={handleSearch} type="button" className='bg-primary font-bold text-white ml-1 py-2 px-4 rounded-lg hover:bg-white border-2 border-primary w-1/4 hover:text-primary transition duration-300'>Search</button>
+            <button onClick={handleSearch} type="button" className='bg-primary font-bold text-white ml-1 py-2 px-4 rounded-lg hover:bg-white border-2 border-primary w-1/4 hover:text-primary transition duration-300'>
+                Search
+            </button>
         </div>
     )
 }

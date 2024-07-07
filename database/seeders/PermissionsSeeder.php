406357509ffb1335447cc8dcbea1b240b1e8729a
Alain Jean <ajean@sugit.com.ar>
2024-07-07 11:13:51 -0400

    updated permission seeder

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -17,12 +17,13 @@ class PermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // create permissions
+        // Create role permissions
         Permission::create(['name' => 'create roles']);
         Permission::create(['name' => 'read roles']);
         Permission::create(['name' => 'edit roles']);
         Permission::create(['name' => 'delete roles']);
 
+        // Create cars permissions
         Permission::create(['name' => 'create cars']);
         Permission::create(['name' => 'read cars']);
         Permission::create(['name' => 'edit cars']);
@@ -30,21 +31,30 @@ class PermissionsSeeder extends Seeder
         Permission::create(['name' => 'publish cars']);
         Permission::create(['name' => 'unpublish cars']);
 
+        // Create contact permissions
+        Permission::create(['name' => 'reply contacts']);
+        Permission::create(['name' => 'delete contacts']);
+
         // create admin role 
         $admin = Role::create(['name' => 'admin']);
 
-        // Assign permissions
+        // Assign role permissions to admin
         $admin->givePermissionTo('create roles');
         $admin->givePermissionTo('read roles');
         $admin->givePermissionTo('edit roles');
         $admin->givePermissionTo('delete roles');
 
+        // Assign car permissions to admin
         $admin->givePermissionTo('create cars');
         $admin->givePermissionTo('read cars');
         $admin->givePermissionTo('edit cars');
         $admin->givePermissionTo('delete cars');
         $admin->givePermissionTo('publish cars');
         $admin->givePermissionTo('unpublish cars');
+
+        // Assign contact permissions to admin
+        $admin->givePermissionTo('reply contacts');
+        $admin->givePermissionTo('delete contacts');
 
         // create admin user
         $adminUser = \App\Models\User::factory()->create([
--- resources/js/Components/hooks/usePermissions.js
+++ resources/js/Components/hooks/usePermissions.js
@@ -2,7 +2,6 @@ import { usePage } from '@inertiajs/react';
 
 export default function usePermissions() {
     const { auth } = usePage().props
-    console.log('Permissions', auth.permissions)
     
     const can = (permission) => {
         return auth.permissions.includes(permission);
